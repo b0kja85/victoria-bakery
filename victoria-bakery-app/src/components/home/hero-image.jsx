88addd3aca381a7d4308c4a1b920349b2b45711b
JKM Kiunisala <jkm.kiunisala@gmail.com>
2024-03-17 16:16:00 +0800

    Added Gradient in the Hero Image

--- victoria-bakery-app/src/components/home/hero-image.jsx
+++ victoria-bakery-app/src/components/home/hero-image.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function HeroImage() {
     return (
-        <div className="hero--hero-image">
-            <img src='../../../public/hero_page_BG.png' className='hero--Image' alt='victoria-bakery-hero-img' />
-        </div>
+        <>        
+            <div className="hero--gradient"></div>
+            <div className="hero--hero-image">
+                <img src='../../../public/hero_page_BG.png' className='hero--Image' alt='victoria-bakery-hero-img' />
+            </div>
+        </>
     )
 }
